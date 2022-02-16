a47147509747d198b7eb9291466a820de8ed7254
JalalHasan-22 <jhasanj8@gmail.com>
2022-02-16 19:14:27 +0200

    Last Task Solved Successfully

--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Movie from "../movie/Movie";
 import "./MovieList.css";
-function MovieList({ movies }) {
+function MovieList({ movies, updateMovie }) {
   return (
     <div className="component">
       <h1>Trending Movies</h1>
       <div className="container">
         {movies.map((movie) => (
-          <Movie key={movie.id} movie={movie} />
+          <Movie key={movie.id} movie={movie} updateMovie={updateMovie} />
         ))}
       </div>
     </div>
